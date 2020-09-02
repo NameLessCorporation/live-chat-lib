2d31c14f512141596e3627a908849365859d3ad7
midaef <danil.present@mail.ru>
2020-09-02 18:41:53 +0300

    update(test)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/NameLessCorporation/live-chat-lib
 
 go 1.15
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518 // indirect
+)
--- test/main.go
+++ test/main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"github.com/NameLessCorporation/live-chat-lib/hub"
-	models "github.com/NameLessCorporation/live-chat-lib/models"
+	"github.com/NameLessCorporation/live-chat-lib/models"
 	websocket "github.com/NameLessCorporation/live-chat-lib/websocket"
 )
 
@@ -17,21 +17,55 @@ func main() {
 }
 
 func handler() {
-	h := hub.NewHub()
-	go h.Run()
+	rooms := models.NewRooms()
+	// var ClientsQueue []*hub.ClientInfo
 	var clientInfo hub.ClientInfo
-	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		ws := websocket.NewWebSocket(h)
-		ws.RunWebSocket(&models.WebSocketConfig{
-			Response: w,
-			Request:  *r,
-			Token:    "12345678",
-		}, &clientInfo)
-	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
-	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		ws := websocket.NewWebSocket(w, r)
+		ws.ConnectionWebSocket(&clientInfo, rooms)
+	})
+
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		type Request struct {
+			Name  string `json:"name"`
+			Token string `json:"token"`
+		}
+		var req Request
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Request error: ", err)
+		}
+		json.Unmarshal(body, &req)
+		room := &models.Room{
+			Name:    req.Name,
+			Token:   req.Token,
+			Clients: nil,
+			Hub:     nil,
+		}
+		rooms.Create(room)
+	})
+
+	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
+		type Request struct {
+			Token string `json:"token"`
+		}
+		var req Request
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Request error: ", err)
+		}
+		json.Unmarshal(body, &req)
+		room := &models.Room{
+			Token: req.Token,
+		}
+		rooms.Delete(room)
+	})
+
+	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Println("Request error: ", err)
